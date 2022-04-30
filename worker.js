2eb4f73c0cc145de6d919136631e32ab52f20242
seonja <seonja.kim0@gmail.com>
2022-04-30 19:58:48 +0900

    feat: build expect function

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const expect = require('expect')
 
 exports.runTest = async (testFile) => {
   const code = await fs.promises.readFile(testFile, 'utf8')
@@ -8,6 +9,14 @@ exports.runTest = async (testFile) => {
     errorMessage: null,
   }
   try {
+    // can build expect like this below
+    // const expect = (received) => ({
+    //   toBe: (expected) => {
+    //     if (received !== expected) {
+    //       throw new Error(`Expected ${expected} but received ${received}`)
+    //     }
+    //   },
+    // })
     eval(code)
     testResult.success = true
   } catch (error) {
