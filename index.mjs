057097f9bc90a89412915ddc1015067000208e99
seonja <seonja.kim0@gmail.com>
2022-04-30 18:22:45 +0900

    feat: initial setting

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import Haste from 'jest-haste-map'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { cpus } from 'os'
+
+// this is __dirname in an .mjs file
+const root = dirname(fileURLToPath(import.meta.url))
+const hasteMap = new Haste.default({
+  rootDir: root,
+  roots: [root],
+  maxWorker: cpus().length, // draw dependecy graph?
+  platforms: [],
+  extensions: ['js'],
+  name: 'my-testing-framework', // cache name
+})
+
+const { hasteFS } = await hasteMap.build()
+
+const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js'])
+console.log(testFiles)
