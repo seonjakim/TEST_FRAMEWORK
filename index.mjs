f1d7ee5d9081d8114ec00a70385376c954c7cee4
seonja <seonja.kim0@gmail.com>
2022-04-30 18:48:11 +0900

    refactor: update the worker

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
 import Haste from 'jest-haste-map'
-import { dirname } from 'path'
+import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import { cpus } from 'os'
+import { Worker } from 'jest-worker'
 
 // this is __dirname in an .mjs file
 const root = dirname(fileURLToPath(import.meta.url))
@@ -18,8 +19,12 @@ const { hasteFS } = await hasteMap.build()
 
 const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js'])
 
-import Worker from './worker.js'
+const worker = new Worker(join(root, 'worker.js'), {
+  enableWorkerThreads: true,
+})
 
 for await (const testFile of testFiles) {
-  console.log(await Worker.runTest(testFile))
+  console.log(await worker.runTest(testFile))
 }
+
+worker.end()
